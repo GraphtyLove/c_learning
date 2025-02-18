2527e1a0cc263ee847b17cfc8c64b75343a498be
Maxim Berge <maxim.berge@gmail.com>
2025-02-18 12:18:42 +0100

    add c05

---
+++ C05/ex07/ft_find_next_prime.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mberge <mberge@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/18 12:09:09 by mberge            #+#    #+#             */
+/*   Updated: 2025/02/18 12:13:16 by mberge           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_is_prime(int nb)
+{
+	int	i;
+
+	if (nb < 2)
+		return (0);
+	if (nb == 2)
+		return (1);
+	if (nb % 2 == 0)
+		return (0);
+	i = 3;
+	while (i <= nb)
+	{
+		if (i != nb && nb % i == 0)
+			return (0);
+		i += 2;
+	}
+	return (1);
+}
+
+int	ft_find_next_prime(int nb)
+{
+	if (ft_is_prime(nb))
+		return (nb);
+	return (ft_find_next_prime(nb + 1));
+}
